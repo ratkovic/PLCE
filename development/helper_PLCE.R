339ac139da59bceb4edee8e2451f319c5efdf3cc
ratkovic <ratkovic@users.noreply.github.com>
2021-06-03 19:15:37 -0400

    Removing development directories
    Development now in another branch

--- development/helper_PLCE.R
+++
@@ -1,3 +0,0 @@
-Rcpp::compileAttributes('~/Dropbox/Github/PLCE')           # this updates the Rcpp layer from C++ to R
-roxygen2::roxygenize('~/Dropbox/Github/PLCE', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
-roxygen2::roxygenize('~/Dropbox/Github/PLCE')  # this updates the documentation based on roxygen comments
