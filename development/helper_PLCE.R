0940f754db344829b4db8e5cd3a8a457292874e9
ratkovic <ratkovic@users.noreply.github.com>
2022-10-03 21:59:05 -0400

    Updating yaml check file

--- development/helper_PLCE.R
+++ development/helper_PLCE.R
@@ -1,3 +1,12 @@
 Rcpp::compileAttributes('~/Dropbox/Github/PLCE')           # this updates the Rcpp layer from C++ to R
 roxygen2::roxygenize('~/Dropbox/Github/PLCE', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
 roxygen2::roxygenize('~/Dropbox/Github/PLCE')  # this updates the documentation based on roxygen comments
+
+
+if(F){
+  # https://r-pkgs.org/release.html
+  library(usethis)
+  library(devtools)
+  devtools::release('~/Downloads/PLCE', check = TRUE)
+  
+}
